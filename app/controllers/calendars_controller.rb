30212c651b423ea74bd16a241b7e69077e3f7292
ryuseikorogi <70619565+ryuseikorogi@users.noreply.github.com>
2020-09-18 12:19:18 +0900

    issue4~6の改称4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -46,6 +46,4 @@ class CalendarsController < ApplicationController
   end
 end
 
-     #   #wday_num = wday_num - 7
-     # end
      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
